950c5ea35831ffcc8676833dc515df5196e713dc
abhiraj123456785212-ctrl <abhiraj123456785212@gmail.com>
2026-06-24 13:45:19 +0530

    Update bot.py
    Signed-off-by: abhiraj123456785212-ctrl <abhiraj123456785212@gmail.com>

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -1,13 +1,14 @@
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
+
 import config
+
 from ..logging import LOGGER
 
 
 class Anony(Client):
     def __init__(self):
-        LOGGER(__name__).info("Starting Bot...")
-
+        LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
             name="AnonXMusic",
             api_id=config.API_ID,
@@ -16,36 +17,38 @@ class Anony(Client):
             in_memory=True,
             parse_mode=ParseMode.HTML,
             max_concurrent_transmissions=7,
-            proxy=config.PROXY if config.PROXY else None,
         )
 
     async def start(self):
         await super().start()
-
         self.id = self.me.id
-        self.name = (self.me.first_name or "") + " " + (self.me.last_name or "")
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
 
         try:
-            if config.LOGGER_ID:
-                await self.send_message(
-                    chat_id=config.LOGGER_ID,
-                    text=f"<b>Bot Started</b>\nID: {self.id}\nName: {self.name}",
-                )
-        except Exception:
-            pass
+            await self.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+            )
+        except (errors.ChannelInvalid, errors.PeerIdInvalid):
+            LOGGER(__name__).error(
+                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+            )
+            exit()
+        except Exception as ex:
+            LOGGER(__name__).error(
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+            )
+            exit()
 
-        try:
-            if config.LOGGER_ID:
-                a = await self.get_chat_member(config.LOGGER_ID, self.id)
-                if a.status != ChatMemberStatus.ADMINISTRATOR:
-                    LOGGER(__name__).error("Bot not admin in log group")
-                    exit()
-        except Exception:
-            pass
-
-        LOGGER(__name__).info(f"Bot Started as {self.name}")
+        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(__name__).error(
+                "Please promote your bot as an admin in your log group/channel."
+            )
+            exit()
+        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
